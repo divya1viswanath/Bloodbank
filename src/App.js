7845680672c9b2cd340d74097edbcfe71f82e884
divya1viswanath <divyarugmini798@gmail.com>
2022-05-27 08:20:18 +0530

    header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Signup from './component/Signup';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Signup/>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'exact element={<Signup/>}/>
+      </Routes>
+    </BrowserRouter>
+  </>
   );
 }
 
